5cc10397037487489611891e3e6562700c10618c
mary0077 <marianagomes201@hotmail.com>
2024-11-25 19:03:50 -0300

    upando as atividades em python

---
+++ atividade02.py
@@ -0,0 +1,35 @@
+"""
+Your module description
+"""
+
+print("Python has three numeric types: int, float, and complex")
+
+myValue=3.14
+
+print(myValue)
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=5j
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+
+print(myValue)
+
+print(type(myValue))
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+
+print(myValue)
+
+print(myValue)
+
+print(str(myValue) + " is of the data type " + str(type(myValue)))
